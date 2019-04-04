4e9ebdba93224dcc540df9516b4dc3be38c8163a
Liam Glanfield <liam@glanfield.co.uk>
2019-04-04 22:23:26 +0100

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,8 @@ from os.path import abspath, exists
 from blacklist import blacklist
 from database import database
 from slack import slack
-import sys, json, hashlib
+from hashlib import sha1
+import sys, json, hmac, subprocess
 
 f_config = abspath("config.json")
 bl = blacklist()
@@ -24,12 +25,12 @@ db.close()
 
 # set vars based on config file
 SERVER_NAME = config.get('server','Automated Pull API Beta')
-SECRET = 'sha1=' + hashlib.sha1(config.get('secret','some-random-token-string')).hexdigest()
+SECRET = config.get('secret','some-random-token-string')
 SLACK_WEBHOOK = config.get('slack_webhook',None)
 BLACKLIST_TIMEOUT = int(config.get('blacklist_expiry_mins',60))
 
 print ' * API starting'
-print ' * Access token is ' + CONFIG_TOKEN
+print ' * Access token is ' + SECRET
 if SLACK_WEBHOOK:
     print 'Slack notifications enabled ' + SLACK_WEBHOOK
     s = slack(SLACK_WEBHOOK)
@@ -59,11 +60,16 @@ def run_prereq_tasks():
         abort(404)
 
     # validate the token before any requests
+    raw = request.data
     data = request.get_json(silent=True)
     req_secret = request.headers.get('X-Hub-Signature')
 
+    # calculate the hmac sha1
+    hashed = 'sha1=' + hmac.new(str.encode(SECRET.encode('utf8')), str.encode(raw), sha1).hexdigest()
+    print 'calculated: ' + hashed
+
     # if token is an empty string or does not match return 403
-    if not req_secret or not req_secret == SECRET:
+    if not req_secret or not req_secret == hashed:
         bl.add(request.remote_addr)
         if SLACK_WEBHOOK:
             s.send_message('The IP ' + request.remote_addr + ' has been blacklisted due to an incorrect token.')
@@ -73,9 +79,26 @@ def run_prereq_tasks():
 @app.route('/push',methods=['POST'])
 def push():
 
+    # check if its a push or ping
+    req_event = request.headers.get('X-GitHub-Event')
+    req_guid = request.headers.get('X-GitHub-Delivery')
+
+    # if push then pull :D
+    if req_event == 'push':
+        c = subprocess.Popen(['sudo','git','pull'],cwd='/opt/monkey-bot',stdout=subprocess.PIPE)
+        git_out, git_err = c.communicate()
+        git_exit = c.wait()
+
+        c = subprocess.Popen(['sudo','systemctl','restart','monkey-bot.service'],stdout=subprocess.PIPE)
+        service_out, service_err = c.communicate()
+        service_exit = c.wait()
+
     # send slack messaage
-    if SLACK_WEBHOOK:
-        s.send_message('A valid push request was sent by GitHub.')
+    if SLACK_WEBHOOK and req_event == 'ping':
+        s.send_message('A valid `' + req_event + '` request was sent by GitHub, signature `' + req_guid + '`.')
+
+    if SLACK_WEBHOOK and req_event == 'push':
+        s.send_message('A valid `' + req_event + '` request was sent by GitHub, signature `' + req_guid + '`.\n\nLocal Repo Pull Result:\n```\n' + git_out + '\n```\n')
 
     # return the result
     out = 'OK'
